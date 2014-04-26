39620542150bc1121db826c924479200cad02fa7
Brian A. Danielak <brian@briandk.com>
2014-04-26 15:55:50 -0500

    Add getCharacterByCharacterID()

--- example.js
+++ example.js
@@ -1,1 +1,19 @@
-var timestamp = new Date().getTime();
+
+var characterID = '1009410';
+
+var requestObject = function() {
+						return(
+							{'ts': '1398541614409',
+					 	 	 'privateKey': '0673db901ec0e86725fa1132310f6f8e6ee75c1c',
+					 	 	 'apikey': '8f45a9ca60fd35092b39873cabac3328',
+					 	 	 'hash': 'a2ee237124cc4dc508e4511382fcd964'})
+					}
+
+
+
+
+var getCharacterByCharacterID = function(characterID) {
+	var url = 'http://gateway.marvel.com/v1/public/characters/' + characterID
+	var requestData = new requestObject()
+	return($.getJSON(url, requestData));
+}
